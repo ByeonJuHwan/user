2952691f08fed1c029e81b878678069cb9800b13
juhwan <asdcz@naver.com>
2024-01-21 21:31:50 +0900

    로그인 이후 회원 이름을 띄우기 위해 이름 추가

--- src/main/java/com/join/user/domain/User.java
+++ src/main/java/com/join/user/domain/User.java
@@ -17,12 +17,14 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    private String name;
     private String userId;
     private String password;
 
     @Builder
-    public User(Long id, String userId, String password) {
+    public User(Long id, String name, String userId, String password) {
         this.id = id;
+        this.name = name;
         this.userId = userId;
         this.password = password;
     }
