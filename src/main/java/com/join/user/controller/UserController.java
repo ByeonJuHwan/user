15ea3be733755b6bee100f3c7fd26631a4b102e9
juhwan <asdcz@naver.com>
2024-01-21 21:32:27 +0900

    로그인, 로그아웃 기능을 위해 MainController 로 분리

--- src/main/java/com/join/user/controller/UserController.java
+++ src/main/java/com/join/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.join.user.controller;
 
-import com.join.user.domain.User;
 import com.join.user.dto.UserDto;
 import com.join.user.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +11,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
@@ -20,11 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final UserService userService;
-
-    @GetMapping("/")
-    public String main() {
-        return "main";
-    }
 
     @GetMapping("/api/join")
     public String joinPage(Model model){
