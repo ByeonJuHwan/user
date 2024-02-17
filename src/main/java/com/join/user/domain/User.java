fbb836ad32187014442005b6e9702aa8d2ddd38f
juhwan <asdcz@naver.com>
2024-02-17 18:01:55 +0900

    시퀀스 제대로 올라가도록 변경

--- src/main/java/com/join/user/domain/User.java
+++ src/main/java/com/join/user/domain/User.java
@@ -14,7 +14,7 @@ import static lombok.AccessLevel.*;
 @NoArgsConstructor(access = PROTECTED)
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
